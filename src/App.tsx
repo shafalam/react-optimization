8b70d936c3198266f458c8777559b78a6f3030ff
Shafiul Alam <shafiulalam16@gmail.com>
2020-08-17 12:34:14 +0200

    experimented with React.memo()

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { v4 as uuidv4 } from "uuid";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+const App = () => {
+  console.log("app component");
+  const [users, setUsers] = React.useState([
+    { id: "a", name: "timmi" },
+    { id: "b", name: "mitu" },
+  ]);
+
+  const [text, setText] = React.useState("");
+
+  const handleText = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setText(event.target.value);
+  };
+
+  const handleAddUser = () => {
+    setUsers(users.concat({ id: uuidv4(), name: text }));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" value={text} onChange={handleText} />
+      <button type="button" onClick={handleAddUser}>
+        Add User
+      </button>
+
+      <List list={users} />
     </div>
   );
+};
+
+interface ListProps {
+  list: any[];
 }
+
+const List = React.memo(({ list }: ListProps) => {
+  console.log("list component");
+  return (
+    <ul>
+      {list.map((item) => (
+        <ListItem key={item.id} item={item} />
+      ))}
+    </ul>
+  );
+});
+
+interface ListItemProps {
+  item: any;
+}
+
+const ListItem = React.memo(({ item }: ListItemProps) => {
+  console.log("list-item component");
+  return <li>{item.name}</li>;
+});
 
 export default App;
